b732c3534521eaefa651bd711480dfa4ef069946
David Figatner <david@yopeyopey.com>
2017-01-06 14:08:20 +0800

    added test code for side of the button

--- index.js
+++ index.js
@@ -3,15 +3,50 @@ const SettingsPanel = require('../settingspanel/settingspanel.js');
 // initializes the Settings Panel
 const panel = new SettingsPanel({background: 'rgb(50,50,50)'});
 
-// create a button that changes its own color on callback
-const button = panel.button('change background', function() { button.style.background = randomColor(); });
+// create a button that changes its own color on callback and increments a counter on the button
+let count = 0;
+const button = panel.button('change button background: ', function() { button.style.background = randomColor(); return count++; });
 
 // create an input panel that changes the test text in the middle of the page
 const text = number();
-panel.input('red ', function(value) { text.innerHTML = value; }, {original: text.innerHTML});
+panel.input('red ',
+    function(value)
+    {
+        text.innerHTML = value;
+    }, {original: text.innerHTML, color: 'red'});
+
+// creates a toggle button that changes the opacity of red
+panel.button('red is visible: ',
+    function()
+    {
+        if (text.style.opacity === '1')
+        {
+            text.style.opacity = '0.2';
+            return 'no';
+        }
+        else
+        {
+            text.style.opacity = '1';
+            return 'yes';
+        }
+    }, {original: 'yes', background: 'rgb(150,0,0'});
+
+let right = true;
+panel.button('side of panel: ',
+    function()
+    {
+        right = !right;
+        panel.side(right ? 'right' : 'left');
+        return right ? 'right' : 'left';
+    }, {original: 'right', background: 'rgb(0,0,150)'});
 
 // create a button with instruction text that does nothing when pressed
-panel.button('<b>Instructions</b>:<br>* press the change background button<br>* alter the value of the red input box<br>* press the triangle to minimize this panel', null, {style: {textAlign: 'left', background: 'green'}});
+panel.button('<b>Instructions for panels</b>:' +
+    '<br>* change background of the button' +
+    '<br>* change the red value' +
+    '<br>* make red visible/invisible' +
+    '<br>* change    the side of the panel' +
+    '<br>* press the triangle to minimize this panel', null, {style: {textAlign: 'left', background: 'green'}});
 
 // create and style test text
 function number()
@@ -23,6 +58,7 @@ function number()
     number.style.position = 'fixed';
     number.style.left = number.style.top = '50%';
     number.style.color = 'red';
+    number.style.opacity = 1;
     return number;
 }
 
